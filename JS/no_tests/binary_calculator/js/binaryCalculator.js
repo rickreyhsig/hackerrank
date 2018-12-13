68492a4f771f39506e7a8455d85db30d536b18df
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2018-12-13 12:35:17 -0500

    Adding HackerRank JS Binary Calculator progress #3

---
+++ JS/no_tests/binary_calculator/js/binaryCalculator.js
@@ -0,0 +1,32 @@
+function clickEql() {
+    res = document.getElementById("res");
+    //res.innerHTML = res.innerHTML+'0';
+}
+function clickZero() {
+    res = document.getElementById("res");
+    res.innerHTML = res.innerHTML+'0';
+}
+function clickOne() {
+    res = document.getElementById("res");
+    res.innerHTML = res.innerHTML+'1';
+}
+function clickClr() {
+    res = document.getElementById("res");
+    res.innerHTML = '';
+}
+function clickSum() {
+    res = document.getElementById("res");
+    res.innerHTML = res.innerHTML+'+';
+}
+function clickSub() {
+    res = document.getElementById("res");
+    res.innerHTML = res.innerHTML+'-';
+}
+function clickMul() {
+    res = document.getElementById("res");
+    res.innerHTML = res.innerHTML+'*';
+}
+function clickDiv() {
+    res = document.getElementById("res");
+    res.innerHTML = res.innerHTML+'/';
+}
