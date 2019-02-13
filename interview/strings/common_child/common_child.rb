7083601fdede2247744321266b118e5d253d7f43
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2019-02-13 14:06:27 -0500

    HackerRank Interview Prep - Strings: Common Child

--- interview/strings/common_child/common_child.rb
+++ interview/strings/common_child/common_child.rb
@@ -1,88 +1,30 @@
 # https://www.hackerrank.com/challenges/common-child/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
-class CommonChild
-    def common_child(s1, s2)
-        t1 = ''
-        t2 = ''
-        s2.each_char.with_index do |ch, i|
-            if s1.include? (ch)
-                t1 << ch
-                t2 << ch
-                s1.slice!(0,s1.index(ch)+1)
-                s2.slice!(i)
-            end
-        end
-        p t1.length
-
-        s1.each_char.with_index do |ch, i|
-            if s2.include? (ch)
-                t1 << ch
-                t2 << ch
-                s2.slice!(0,s2.index(ch)+1)
-                s2.slice!(i)
-            end
-        end
-        p t2.length
-
-
-        return t1.length
-    end
-end
-
-=begin
-class CommonChild
+#class CommonChild
 #    def common_child(s1, s2)
-#        cnt = 0
 #        t1 = ''
 #        t2 = ''
 #        s2.each_char.with_index do |ch, i|
-#            #p ch
-#            #p "#{s1.count(ch)}:#{s2.count(ch)}"
 #            if s1.include? (ch)
-#                cnt += 1
 #                t1 << ch
 #                t2 << ch
-#                #s1.slice!(0,s1.index(ch)+1)
-#                #s2.slice!(i)
+#                s1.slice!(0,s1.index(ch)+1)
+#                s2.slice!(i)
 #            end
-#            p "s1:#{s1}-s2:#{s2}"
-#            #p "t1:#{t1}-t2:#{t2}"
 #        end
-#        p "cnt: #{cnt}"
 #        return t1.length
 #    end
+#end
+
+class CommonChild
     def common_child(s1, s2)
-        p s1
-        p s2
-        cnt = 0
-        p s1.length
-        s2.each_char.with_index do |ch, i|
-
-            s1.gsub(ch, '') unless s2.include? ch
-            s2.gsub(ch, '') unless s1.include? ch
-
-            p "#{s1.count(ch)}:#{s2.count(ch)}"
-            if s1.count(ch) > s2.count(ch)
-                a = s1.count(ch) - s2.count(ch)
-                a.times { |i| s1.slice!(ch) }
-            elsif s2.count(ch) > s1.count(ch)
-                b = s2.count(ch) - s1.count(ch)
-                b.times { |i| s2.slice!(ch) }
-            end
-
-            if s1.include? (ch)
-                cnt += 1
-
-                #t1 << ch
-                #t2 << ch
-                #s1.slice!(0,s1.index(ch)+1)
-                #s2.slice!(i)
-            end
+      m = Array.new(s1.size + 1, 0)
+      for i in 1..s1.size
+        new = Array.new(1, 0)
+        for j in 1..s2.size
+          new.push s1[i-1] == s2[j-1] ? m[j-1] + 1 : [new[j-1], m[j]].max
         end
-        p s1
-        p s2
-        p s1.length
-        p "cnt: #{cnt}"
-        return s2.length
+        m = new
+      end
+      m.last
     end
 end
-=end
--- interview/strings/common_child/tc_common_child.rb
+++ interview/strings/common_child/tc_common_child.rb
@@ -4,18 +4,14 @@ require "test/unit"
 class TestCommonChild < Test::Unit::TestCase
     def test_simple
         c = CommonChild.new
-        #assert_equal(3, c.common_child('ABCD', 'ABDC'))
-        #p '******'
-        #assert_equal(2, c.common_child('HARRY', 'SALLY'))
-        #p '******'
-        #assert_equal(3, c.common_child('SHINCHAN', 'NOHARAAA'))
-        #p '******'
-        #assert_equal(0, c.common_child('AA', 'BB'))
+        assert_equal(3, c.common_child('ABCD', 'ABDC'))
+        assert_equal(2, c.common_child('HARRY', 'SALLY'))
+        assert_equal(3, c.common_child('SHINCHAN', 'NOHARAAA'))
+        assert_equal(0, c.common_child('AA', 'BB'))
     end
 
     def test_longer
         c = CommonChild.new
         assert_equal(15, c.common_child('WEWOUCUIDGCGTRMEZEPXZFEJWISRSBBSYXAYDFEJJDLEBVHHKS', 'FDAGCXGKCTKWNECHMRXZWMLRYUCOCZHJRRJBOAJOQJZZVUYXIC'))
-        #p '******'
     end
 end
