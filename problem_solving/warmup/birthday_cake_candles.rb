1599bc419db41274cb37894a38374c4c8759dd25
Ricardo Kreyhsig <rkreyhsig@MotoRefis-MacBook-Pro.local>
2019-02-19 20:49:45 -0500

    HackerRank Problem Solving - Warmup: Birthday Cake Candles

---
+++ problem_solving/warmup/birthday_cake_candles.rb
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar)
+  return ar.count(ar.max)
+end
+
+p birthdayCakeCandles([4,4,1,3])
+p birthdayCakeCandles([2,3,1,3])
